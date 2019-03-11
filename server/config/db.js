3dce2d5a85c46bf01cb0cffe459df8756e035f3f
ZiqingQiu <qziqing@gmail.com>
2019-03-10 20:35:36 -0400

    add week 9 doc

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-   //"URI" : "mongodb://localhost/test"
-    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b"
+    "URI" : "mongodb://localhost/test"
+    //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b"
+    //"URI": "mongodb+srv://admin:Zhaixf201902@cluster0-9ytpk.mongodb.net/test?retryWrites=true" 
 }
