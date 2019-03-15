9798c2983ebf9d7f4d2954855881992f8ef7040f
ZiqingQiu <qziqing@gmail.com>
2019-03-15 15:56:39 -0400

    add dropdown to register, refine code

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,18 +1,24 @@
 // IIFE -- Immediately Invoked Function Expression
-(function(){
+(function () {
 
     function Start() {
-        console.log(`%c App Started...`, 
-        "font-size: 20px; color: blue; font-weight: bold");
+        console.log(`%c App Started...`,
+            "font-size: 20px; color: blue; font-weight: bold");
 
-        $(".btn-danger").click(function(event) {
-            if(!confirm("Are you sure???")) {
+        $(".dropdown-menu a").click(function () {
+            $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+            $(this).parents(".dropdown").find('.btn').val($(this).text());
+            $(this).parents(".dropdown").find('.form-control').val($(this).text());
+        });
+
+        $(".btn-danger").click(function (event) {
+            if (!confirm("Are you sure???")) {
                 event.preventDefault();
                 window.location.assign("/order-list");
             }
         });
     }
-    
+
     window.addEventListener("load", Start);
 
 })();
