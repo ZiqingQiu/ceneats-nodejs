e8b9d53a389e9ef876d5779898e3e7856dd46c7e
ZiqingQiu <qziqing@gmail.com>
2019-10-20 21:45:31 -0400

    store post request successfully

--- public/sw_cached_site.js
+++ public/sw_cached_site.js
@@ -1,4 +1,50 @@
 const cacheName = 'v2';
+const INDEX_DB_NAME = 'ceneats_form'
+const OBJSTORE_POST_REQ_NAME = 'post_requests'
+
+// Index DB
+let post_req_db;
+let form_data;
+function createIndexDB() {
+  let indexedDBOpenRequest = indexedDB.open(INDEX_DB_NAME, 1);
+  indexedDBOpenRequest.onerror = function (error) {
+    // error creating db
+    console.error('IndexedDB error:', error)
+  }
+  indexedDBOpenRequest.onupgradeneeded = function () {
+    // This should only executes if there's a need to 
+    // create/update db.
+    this.result.createObjectStore(OBJSTORE_POST_REQ_NAME, {
+      autoIncrement: true, keyPath: 'id'
+    })
+  }
+  // This will execute each time the database is opened.
+  indexedDBOpenRequest.onsuccess = function () {
+    console.log('post_req_db success inited...');
+    post_req_db = this.result
+  }
+}
+
+function getObjectStore(objStoreName, mode) {
+  // retrieve our object store
+  return post_req_db.transaction(objStoreName, mode
+  ).objectStore(objStoreName)
+}
+
+function savePostRequests(url, payload) {
+  // get object_store and save our payload inside it
+  var request = getObjectStore(OBJSTORE_POST_REQ_NAME, 'readwrite').add({
+    url: url,
+    payload: payload,
+    method: 'POST'
+  })
+  request.onsuccess = function (event) {
+    console.log('a new pos_ request has been added to indexedb')
+  }
+  request.onerror = function (error) {
+    console.error(error)
+  }
+}
 
 // Call Install Event
 self.addEventListener('install', e => {
@@ -22,6 +68,8 @@ self.addEventListener('activate', e => {
       );
     })
   );
+  // Create Index DB
+  e.waitUntil(createIndexDB());
 });
 
 // Call Fetch Event
@@ -46,10 +94,19 @@ self.addEventListener('fetch', e => {
   else if (e.request.clone().method === 'POST') {
     console.log('Service Worker: Fetching -- POST');
         // attempt to send request normally
-        e.respondWith(fetch(e.request.clone()).catch(function
-          (error) {
+        e.respondWith(fetch(e.request.clone())
+        .catch(err => {
             // only save post requests in browser, if an error occurs
-            //savePostRequests(e.request.clone().url, form_data)
+            savePostRequests(e.request.clone().url, form_data)
           }))
   }
 });
+
+// Capture message event
+self.addEventListener('message', e => {
+  console.log('form data', e.data)
+  if (e.data.hasOwnProperty('form_data')) {
+    // receives form data from post js upon submission
+    form_data = e.data.form_data
+  }
+})
